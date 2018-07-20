9bb7a69b5cfe938be473871c42e9bcfd3e252528
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-07-20 09:08:55 +0200

    Correction variable non detectée

--- nginx-autoinstall.sh
+++ nginx-autoinstall.sh
@@ -69,6 +69,7 @@ case $OPTION in
 		rm default
 		cd /etc/nginx/sites-available
 		rm default
+		wget https://raw.githubusercontent.com/Whisper40/script-nginx-et-autre/nginx.conf -P /etc/nginx/sites-available/
 		
 
 
@@ -98,7 +99,7 @@ case $OPTION in
 		        try_files $uri =404;
 		         fastcgi_index index.php;
 		         fastcgi_pass unix:/var/run/php/php7.2-fpm.sock;
-		         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+		         fastcgi_param SCRIPT_FILENAME "$document_root$fastcgi_script_name";
 		         include /etc/nginx/fastcgi_params;
 		 }
 		}
